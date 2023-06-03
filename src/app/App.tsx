13960e03158ace693c0ed395e63254ce3726b89e
artur-makeev <artur.makeev.d@gmail.com>
2023-06-03 20:08:20 +0300

    Feature-Sliced Design

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,10 @@
 import { Link, Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
-import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
-import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
 import { Suspense } from 'react';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './hekpers/classNames/classNames';
-
-
+import { useTheme } from 'app/providers/ThemeProvider';
+import { classNames } from 'helpers/classNames/classNames';
+import { AboutPage } from 'pages/AboutPage/';
+import { MainPage } from 'pages/MainPage/';
 
 export const App = () => {
 	const {theme, toggleTheme} = useTheme();
@@ -19,8 +17,8 @@ export const App = () => {
 			<Suspense fallback={<div>loading ...</div>}>
 				<Routes>
 				
-					<Route path='/about' element={<AboutPageLazy />}/>
-					<Route path='/' element={<MainPageLazy />}/>
+					<Route path='/about' element={<AboutPage />}/>
+					<Route path='/' element={<MainPage />}/>
 			
 				</Routes>
 			</Suspense>
--- src/components/Counter.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Counter.module.scss';
-
-
-export const Counter = () => {
-	return (
-		<div className={styles.red}>
-			asd
-		</div>
-	)};
